53f6c70de7369c865f8c1d26ebcc90095c57bb75
VictorAlagwu <victoralagwu@gmail.com>
2018-09-03 10:34:55 +0100

    Quote project start

--- advan/src/App.vue
+++ advan/src/App.vue
@@ -2,10 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
-                   <h3 slot="title">{{ quoteTitle }}</h3>
-                   <p >Learn today, Learn tomorrom</p>
-               </app-quote>
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <h3 slot="title">{{ quoteTitle }}</h3>
+                        <p>Learn today, Learn tomorrom</p>
+                    </component>
+                </keep-alive>
+                
+              
             </div>
         </div>
     </div>
@@ -13,15 +22,21 @@
 
 <script>
 import Quote from './components/Quote.vue';
+import Author from './components/Author.vue';
+import New from './components/New.vue';
+
     export default {
         data: () => {
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
             }
             
         },
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
---
+++ advan/src/components/New.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>My New Quote</h3>
+        <button @click="counter++">Increase Counter</button>
+        <p>{{ counter }} </p>
+    </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return{
+            counter: 0
+        };
+    }
+}
+</script>
+<style>
+
+</style>
+
