dda9f170af0c40e57331c02d715d966e7e091946
VictorAlagwu <victoralagwu@gmail.com>
2018-09-14 18:07:21 +0100

    Adding New Page

--- quotes/src/App.vue
+++ quotes/src/App.vue
@@ -1,12 +1,33 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded = "newQuote($event)"></app-new-quote>
+        <hr>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
     export default {
-        
+        data: function () { 
+            return {
+                quotes: [
+                    'Just a quote',
+                    'Another qUOTE'
+                ],
+                maxQuotes: 10
+            }
+         },
+         methods: {
+             newQuote(quote){
+                 this.quotes.push(quote);
+             }
+         },
+         components: {
+             appQuoteGrid: QuoteGrid,
+             appNewQuote: NewQuote
+         }
     }
 </script>
 
---
+++ quotes/src/components/NewQuote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <label for="quote">Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <br>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <button type="submit" @click.prevent="addQuote" class="btn btn-primary">Add Quote</button>
+            </div>
+        </form>
+        
+    </div>
+</template>
+<script>
+export default {
+    data: function() {
+        return {
+            quote: ''
+        }
+    },
+    methods: {
+        addQuote: function(){
+            this.$emit('quoteAdded', this.quote);
+            this.quote = '';
+        }
+    }
+}
+</script>
+<style>
+
+</style>
