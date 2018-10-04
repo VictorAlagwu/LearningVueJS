c0a7e9d8a5a2ae1a86f334ca4a646a89d4083821
VictorAlagwu <victoralagwu@gmail.com>
2018-10-04 10:48:31 +0100

    Directives

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -3,7 +3,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives</h1>
-
+                <p v-text="'Your Boy is Learning How to Use Directives'"></p>
+                <p v-html="'<i>Hello Wo\'rld</i>'"></p>
+                <p v-local-highlight:background.delayed = "'red'">This TEXT IS supposed to be Green in color</p>
             </div>
         </div>
     </div>
@@ -11,6 +13,23 @@
 
 <script>
     export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                     var delay = 0;
+                    if(binding.modifiers['delayed']){
+                        delay = 3000;
+                    }   
+                    setTimeout(() => {
+                    if (binding.arg == 'background') {
+                        el.style.backgroundColor = binding.value;
+                    } else {
+                        el.style.color = binding.value;
+                    }
+                    }, delay);
+                }
+            }
+        }
     }
 </script>
 
