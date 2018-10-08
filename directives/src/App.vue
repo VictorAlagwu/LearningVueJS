aafef50686411a81c1a155ef5527551f56c0497c
VictorAlagwu <victoralagwu@gmail.com>
2018-10-08 10:30:24 +0100

    Filters and  Mixin

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -4,8 +4,8 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives</h1>
                 <p v-text="'Your Boy is Learning How to Use Directives'"></p>
-                <p v-html="'<i>Hello Wo\'rld</i>'"></p>
-                <p v-local-highlight:background.delayed = "'red'">This TEXT IS supposed to be Green in color</p>
+                <!-- <p v-highlight="'<i>Hello Wo\'rld</i>'"></p> -->
+                <p v-local-highlight:background.delayed.blink = "{mainColor: 'red', secondColor: 'green', delay: 500}">This TEXT IS supposed to be Green in color</p>
             </div>
         </div>
     </div>
@@ -17,16 +17,34 @@
             'local-highlight': {
                 bind(el, binding, vnode) {
                      var delay = 0;
-                    if(binding.modifiers['delayed']){
+                    if(binding.modifiers['delayed']) {
                         delay = 3000;
-                    }   
-                    setTimeout(() => {
-                    if (binding.arg == 'background') {
-                        el.style.backgroundColor = binding.value;
+                    } 
+                    if(binding.modifiers['blink']) {
+                        let mainColor = binding.value.mainColor;
+                        let secondColor = binding.value.secondColor;
+                        let currectColor = mainColor;    
+                        setTimeout(() => {
+                            setInterval(() => {
+                                currectColor == secondColor ? currectColor = mainColor : currectColor = secondColor;
+                                if (binding.arg == 'background') {
+                                    el.style.backgroundColor = currectColor;
+                                } else {
+                                    el.style.color = currectColor;
+                                }
+
+                            }, binding.value.delay);
+                        }, delay);
                     } else {
-                        el.style.color = binding.value;
+                        setTimeout(() => {
+                            if (binding.arg == 'background') {
+                                el.style.backgroundColor = binding.value.mainColor;
+                            } else {
+                                el.style.color = binding.value.mainColor;
+                            }
+                        }, delay);
                     }
-                    }, delay);
+                    
                 }
             }
         }
