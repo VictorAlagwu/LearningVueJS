e7f080ea5a9b7e4374a93765c94322d5fe6dd9f7
VictorAlagwu <victoralagwu@gmail.com>
2018-10-22 09:53:55 +0100

    Using Firebase and VueJS

--- server/src/App.vue
+++ server/src/App.vue
@@ -2,14 +2,44 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
+                <h1>VueJs Routing</h1>
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label for="">Mail</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <button class="btn btn-info" @click="fetchInfo">Fetch Data</button>
             </div>
+            
         </div>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: ''
+                }
+            }
+        },
+        methods: {
+            submit() {
+                this.$http.post('https://vuejs-demo-d6820.firebaseio.com/data.json', this.user)
+                    .then (response => {
+                        console.log(response);
+                    }, error => {
+                        console.log (error);
+                    });
+                ;
+            }
+        }
     }
 </script>
 
