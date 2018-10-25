b80edb877c181a44c153327a708f0491c04077c9
VictorAlagwu <victoralagwu@gmail.com>
2018-10-25 10:44:06 +0100

    Custom Resource

--- server/src/App.vue
+++ server/src/App.vue
@@ -12,6 +12,9 @@
                     <input type="text" class="form-control" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br>
                 <button class="btn btn-info" @click="fetchInfo">Fetch Data</button>
                 <hr>
                 <ul class="list-group">
@@ -34,42 +37,50 @@
                     email: ''
                 },
                 users: [],
-                resource: {}
+                resource: {},
+                node: 'data'
             }
         },
         methods: {
             submit() {
-                // this.$http.post('data.json', this.user)
-                //     .then (response => {
-                //         console.log(response);
-                //     }, error => {
-                //         console.log (error);
-                //     });
                 this.resource.save({}, this.user);
                 this.resource.saveAlt(this.user);
-                
             },
             fetchInfo() {
-                this.$http.get('data.json')
-                        .then (response => {
-                            return response.json();
-                        }, error => {
-                            console.log(error);
-                        })
-                        .then (data => {
-                            const resultArray = [];
-                            for (const key in data) {
-                                resultArray.push(data[key]);
-                            }
-                            this.users = resultArray;
-                        });
+                // this.$http.get('data.json')
+                //         .then (response => {
+                //             return response.json();
+                //         }, error => {
+                //             console.log(error);
+                //         })
+                //         .then (data => {
+                //             const resultArray = [];
+                //             for (const key in data) {
+                //                 resultArray.push(data[key]);
+                //             }
+                //             this.users = resultArray;
+                //         });
+                this.resource.getData({node: this.node})
+                    .then (response => {
+                                return response.json();
+                            }, error => {
+                                console.log(error);
+                            })
+                            .then (data => {
+                                const resultArray = [];
+                                for (const key in data) {
+                                    resultArray.push(data[key]);
+                                }
+                                this.users = resultArray;
+                            });
             }
         },
         created() {
             const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'}
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
             };
-                this.resource = this.$resource('data.json', {}, customActions);
+                this.resource = this.$resource('{node}.json', {}, customActions);
         },
     }
 </script>
