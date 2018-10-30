131a11ea4dfb32e116199a26c0ad9e7ca4928162
VictorAlagwu <victoralagwu@gmail.com>
2018-10-30 11:12:27 +0100

    Routing

--- route/src/components/Home.vue
+++ route/src/components/Home.vue
@@ -4,7 +4,17 @@
         <hr>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aut autem, blanditiis consequuntur
             dolore excepturi laborum maiores minima nihil non nulla obcaecati quas quibusdam quod sed suscipit vero
-            voluptatem.</p>
-
+            voluptatem.
+        </p>
+        <button @click="navigateToUser" class="btn btn-primary">Go to User Page</button>
     </div>
 </template>
+<script>
+export default {
+    methods: {
+        navigateToUser() {
+            this.$router.push('/user');
+        }
+    }
+}
+</script>
--- route/src/components/user/UserDetail.vue
+++ route/src/components/user/UserDetail.vue
@@ -1,3 +1,9 @@
 <template>
+<div>
     <h3>Some User Details</h3>
+      <p>
+        Loaded User ID {{ $route.params.id }}
+    </p>
+    <router-link :to="{name: 'userEdit', params: {id: $route.params.id} }" tag="button" class="btn btn-default">Edit User</router-link>
+</div>
 </template>
