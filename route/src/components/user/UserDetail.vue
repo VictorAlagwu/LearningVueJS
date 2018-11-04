c8c1a3286be515e5a7f2922ced4c00cb5b795ece
VictorAlagwu <victoralagwu@gmail.com>
2018-11-04 10:44:36 +0100

    Route

--- route/src/components/user/UserDetail.vue
+++ route/src/components/user/UserDetail.vue
@@ -9,3 +9,32 @@
     </router-link>
 </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+            link: {
+                name: 'userEdit',
+                params: {
+                    id: this.$route.params.id
+                },
+                query: {
+                    locale: 'en',
+                    q: 100
+                },
+                hash: '#data'
+            },
+            aut: false,
+            name: 'Victor'
+        }
+    }
+    // beforeRouteEnter (to, from, next) {
+    //     if (false) {
+    //          next();
+    //     } else {
+    //         next('false');
+    //     }
+    
+    // }
+}
+</script>
